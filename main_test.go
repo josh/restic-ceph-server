f80e979415726e99a379bda83c01146f51490bfe
Joshua Peek <josh@users.noreply.github.com>
2025-06-22 20:06:09 -0700

    Add testscript

--- main_test.go
+++ main_test.go
@@ -1,7 +1,22 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestMain(t *testing.T) {
-	t.Log("Hello, World!")
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+func TestMain(m *testing.M) {
+	testscript.Main(m, map[string]func(){
+		"restic-ceph-server": main,
+	})
+}
+
+func TestScript(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Dir:             "testdata",
+		ContinueOnError: true,
+		UpdateScripts:   os.Getenv("CI") != "1",
+	})
 }
